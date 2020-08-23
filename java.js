50bf6e4f8dd78b10e63fd6882f7cba0ac22e29b9
gen1n <rohansharma88@gmail.com>
2020-08-23 21:20:56 +0530

    excelToJson

---
+++ java.js
@@ -0,0 +1,30 @@
+let selectedFile;
+console.log(window.XLSX);
+document.getElementById('u17_inpu').addEventListener("change", (event) => {
+    selectedFile = event.target.files[0];
+});
+
+let data=[{
+    "name":"jayanth",
+    "data":"scd",
+    "abc":"sdef"
+}];
+
+
+document.getElementById('u18_text').addEventListener("click", () => {
+    XLSX.utils.json_to_sheet(data, 'out.xlsx');
+    if(selectedFile){
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=>{
+         let data = event.target.result;
+         let workbook = XLSX.read(data,{type:"binary"});
+         console.log(workbook);
+         workbook.SheetNames.forEach(sheet => {
+              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+              console.log(rowObject);
+              document.getElementById("u20_text").innerHTML = JSON.stringify(rowObject,undefined,4)
+         });
+        }
+    }
+});
