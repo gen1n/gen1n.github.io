b7186538c20c499ef7f1cbe695317c239804aab7
gen1n <rohansharma88@gmail.com>
2020-08-24 09:37:25 +0530

    Update java.js

--- java.js
+++ java.js
@@ -6,11 +6,7 @@ document.getElementById('u17_input').addEventListener("change", (event) => {
     selectedFile = event.target.files[0];
 });
 
-let data=[{
-    "name":"jayanth",
-    "data":"scd",
-    "abc":"sdef"
-}];
+var data=[{}];
 
 
 document.getElementById('u18').addEventListener("click", () => {
@@ -21,19 +17,21 @@ document.getElementById('u18').addEventListener("click", () => {
         fileReader.onload = (event)=>{
          let data = event.target.result;
          let workbook = XLSX.read(data,{type:"binary"});
-            console.log("--------------");
-            console.log(data);
-            console.log("--------------");
-         console.log(workbook);
+            
+            
          workbook.SheetNames.forEach(sheet => {
               let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
               console.log(rowObject);
               console.log("--------------");
               document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-             data=JSON.stringify(rowObject,undefined,4);
-             console.log("data:-----");
-             console.log(data);
+             data=rowObject;
          });
+            console.log("showing data");
+            title = (data[0].Title);
+            dom1 = data[0].DomainName;
+            dom2 = data[1].DomainName;
+            dom3 = data[2].DomainName;
+            dom4 = data[3].DomainName;
         }
     }
 });
