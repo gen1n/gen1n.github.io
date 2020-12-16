60ea2d02cf36a1344a3a076d589ec4105802807a
gen1n <rohansharma88@gmail.com>
2020-12-16 19:07:43 +0530

    Add files via upload

---
+++ Christmas/SeasonsGreetings2020/v03/playAnim.js
@@ -0,0 +1,20 @@
+"use strict";
+var select = function(s) {
+    return document.querySelector(s);
+  },
+  selectAll = function(s) {
+    return document.querySelectorAll(s);
+  }, 
+  animationWindow = select('#animContainer'),    
+    animData = {
+		wrapper: animationWindow,
+		animType: 'svg',
+		loop: false,
+		prerender: false,
+		autoplay: true,
+		path: 'data.json'
+	}, anim;
+
+
+anim = lottie.loadAnimation(animData);
+ anim.addEventListener('DOMLoaded', onDOMLoaded);
