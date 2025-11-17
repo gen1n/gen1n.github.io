f4848c6adecfc44f42306da6c0af85e72f5400de
gen1n <rohansharma88@gmail.com>
2025-11-17 12:35:57 +0530

    Add files via upload

---
+++ Gravity/GravityEngageTest/landscape/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2363021.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 263359446, 263359447, 263359445, 263359931, 263580128, 263597707 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/263359464/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/263359468/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/263359459/1/ammo.js', 'preload' : true},
+];
